a0afdab4e8869602ceec66849e8acb31958d5ab9
SeoJoonMin <145459802+SeoJoonMin@users.noreply.github.com>
2025-03-16 12:53:57 +0900

    Time: 143 ms (5.02%), Space: 14 MB (92.59%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         // Brute Force 방식 (직접 작성한 답) 
-        // std::vector<int> output;
+        std::vector<int> output;
 
-        // for(int i=0; i<nums.size(); i++){
-        //     for(int j=0; j<nums.size(); j++){
-        //         if(i != j && nums[i] + nums[j] == target){
-        //             return {i, j};
-        //         }
-        //     }
-        // }
+        for(int i=0; i<nums.size(); i++){
+            for(int j=0; j<nums.size(); j++){
+                if(i != j && nums[i] + nums[j] == target){
+                    return {i, j};
+                }
+            }
+        }
 
-        // return output;
+        return output;
 
         // Two-pass Hash Table 방식
         // unordered_map<int, int> hash;
@@ -29,15 +29,15 @@ public:
         // return {};
 
         // One-pass Hash Table 방식
-        unordered_map<int, int> hash;
-        for (int i = 0; i < nums.size(); ++i) {
-            int complement = target - nums[i];
-            if (hash.find(complement) != hash.end()) {
-                return {hash[complement], i};
-            }
-            hash[nums[i]] = i;
-        }
+        // unordered_map<int, int> hash;
+        // for (int i = 0; i < nums.size(); ++i) {
+        //     int complement = target - nums[i];
+        //     if (hash.find(complement) != hash.end()) {
+        //         return {hash[complement], i};
+        //     }
+        //     hash[nums[i]] = i;
+        // }
         // Return an empty vector if no solution is found
-        return {};
+        // return {};
     }
 };
